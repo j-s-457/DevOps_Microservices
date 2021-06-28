9d743d5e1680a571c54e13339ec52d1474646c27
Ubuntu <ubuntu@ip-172-31-6-7.us-east-2.compute.internal>
2021-06-28 03:41:16 +0000

    committing

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=j-s-457/project-ml-microservice-kubernetes .
+docker build -t dr146457/project-ml-microservice-kubernetes .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 j-s-457/project-ml-microservice-kubernetes
+docker run -p 8000:80 dr146457/project-ml-microservice-kubernetes
