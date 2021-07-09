7eae1d808120e595369b6c2468db87fcd594eea6
Ubuntu <ubuntu@ip-172-31-6-7.us-east-2.compute.internal>
2021-07-09 02:37:35 +0000

    upload_docker.sh works

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,13 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-# dockerpath="dr146457/api ."
-# or dockerpath="dr146457/project-ml-microservice-kubernetes"
+dockerpath="dr146457/project-ml-microservice-kubernetes"
 
 # Step 2:  
 # Authenticate & tag
-#docker login --username=dr146457
+docker login --username=dr146457
 echo "Docker ID and Image: $dockerpath"
+docker image tag udacity_project_four $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
+
